8a41cfdce8c75965835f8f1bc674aafd855bb381
TahmidTausif <tahmidtausif49@gmail.com>
2025-04-23 01:30:13 +0600

    update

--- src/app/dashboard/(dashboardLayout)/profile/page.tsx
+++ src/app/dashboard/(dashboardLayout)/profile/page.tsx
@@ -1,9 +1,11 @@
+import ProfileCard from '@/components/dashboard/profile/ProfileCard';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            
+            <h1 className="text-center font-bold text-2xl lg:text-3xl my-5">My Profile</h1>
+            <ProfileCard/>
         </div>
     );
 };
